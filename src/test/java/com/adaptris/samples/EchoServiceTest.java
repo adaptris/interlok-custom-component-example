093017254756a085b257811b3ff608a19b6edff0
Co-Din <din.stolak@proagrica.com>
2020-01-30 15:52:36 +0000

    Missing Test annotation added.

--- src/test/java/com/adaptris/samples/EchoServiceTest.java
+++ src/test/java/com/adaptris/samples/EchoServiceTest.java
@@ -1,5 +1,7 @@
 package com.adaptris.samples;
 
+import org.junit.Test;
+
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.ServiceCase;
 
@@ -13,6 +15,7 @@ public class EchoServiceTest extends ServiceCase {
   protected void setUp() throws Exception {
   }
   
+  @Test
   public void testService() throws Exception {
     execute(new EchoService(), AdaptrisMessageFactory.getDefaultInstance().newMessage());
   }
