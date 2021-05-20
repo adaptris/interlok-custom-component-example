91db7905e45953a0cd23a466f03f7942a390b6c3
Ashley Morgan Anderson <amanderson@albinoloverats.net>
2021-05-20 11:51:26 +0100

    INTERLOK-3771 Update example service
    * Update README to refer to Java 11 and Interlok 4
    * Update example service
      - Include a couple annotations
      - Use lombok
    * Update test accordingly

--- src/main/java/com/adaptris/samples/EchoService.java
+++ src/main/java/com/adaptris/samples/EchoService.java
@@ -1,14 +1,32 @@
 
 package com.adaptris.samples;
 
+import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
 import com.adaptris.core.*;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.StandardCharsets;
 
 @XStreamAlias("echo-service")
+@AdapterComponent
+@ComponentProfile(summary = "Simply put some given string into the message payload", tag = "echo,example,sample")
+@DisplayOrder(order = { "echoMessage" })
 public class EchoService extends ServiceImp {
-  public void doService(AdaptrisMessage msg) throws ServiceException {
-    System.out.println("Hello World");
+
+  static final String DEFAULT = "Hello World.";
+
+  @Getter
+  @Setter
+  private String echoMessage;
+
+  public void doService(AdaptrisMessage message) throws ServiceException {
+    message.setContent(echoMessage(), message.getContentEncoding());
   }
 
   public final void prepare() {
@@ -18,5 +36,9 @@ public class EchoService extends ServiceImp {
   }
 
   public final void closeService() {
+  }
+
+  private String echoMessage() {
+    return StringUtils.defaultIfEmpty(echoMessage, DEFAULT);
   }
 }
--- src/test/java/com/adaptris/samples/EchoServiceTest.java
+++ src/test/java/com/adaptris/samples/EchoServiceTest.java
@@ -1,23 +1,42 @@
 package com.adaptris.samples;
 
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.Service;
+import com.adaptris.interlok.junit.scaffolding.services.ExampleServiceCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.ServiceCase;
 
+import static com.adaptris.samples.EchoService.DEFAULT;
+import static org.junit.Assert.assertEquals;
 
-public class EchoServiceTest extends ServiceCase {
 
-  public EchoServiceTest() {
-    super();
-  }
+public class EchoServiceTest extends ExampleServiceCase {
 
-  protected void setUp() throws Exception {
+  private static final String ALTERNATIVE_MESSAGE = "Goodbye World.";
+
+  private AdaptrisMessage message;
+  private EchoService service;
+
+  @Before
+  public void setUp() throws Exception {
+    message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+    service = new EchoService();
   }
   
   @Test
   public void testService() throws Exception {
-    execute(new EchoService(), AdaptrisMessageFactory.getDefaultInstance().newMessage());
+    execute(service, message);
+    assertEquals(DEFAULT, message.getContent());
+  }
+
+  @Test
+  public void testOwnMessage() throws Exception {
+    service.setEchoMessage(ALTERNATIVE_MESSAGE);
+    execute(service, message);
+    assertEquals(ALTERNATIVE_MESSAGE, message.getContent());
   }
 
   @Override
